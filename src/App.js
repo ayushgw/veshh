25ceffc37d6090a312311569ce7cf2c1179d4154
ayushgw <ayushgoswami@zoho.com>
2022-11-17 18:01:29 +0530

    category item component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './categories.styles.scss'
+import React from 'react';
+import Categories from './components/Categories/Categories';
 
 const App = () => {
   const categories = [
@@ -30,17 +31,7 @@ const App = () => {
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div className="category-container" key={id}>
-          <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}/>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Categories categories={categories} />
   );
 }
 
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CategoryItem from '../CategoryItem/CategoryItem'
+
+import './Categories.scss'
+
+const Categories = ({ categories }) => {
+
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  )
+}
+
+export default Categories
