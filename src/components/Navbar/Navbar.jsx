15cf2143a72f02a0bba4937aba2ecf93164b0365
ayushgw <ayushgoswami@zoho.com>
2022-11-20 15:28:15 +0530

    add navbar logo + styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './Navbar.scss'
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link className="logo" to="/">LOGO</Link>
+      <Link className="logo-container" to="/">
+        <Logo className='logo' />
+      </Link>
       <div className="nav-links">
         <Link className="nav-link" to="shop">SHOP</Link>
         {/* <Link className="nav-link"></Link> */}
