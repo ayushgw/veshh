81b3f08a7271de15bceb0af6505d98e3aeea7420
ayushgw <ayushgoswami@zoho.com>
2022-11-20 22:08:21 +0530

    add google signin using firebase

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,16 +10,41 @@ const firebaseConfig = {
     storageBucket: "crwn-clothing-db-2aae0.appspot.com",
     messagingSenderId: "621442694528",
     appId: "1:621442694528:web:a8517157de3ca55f069565"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+};
 
-  const provider = new GoogleAuthProvider();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  provider.setCustomParameters({
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
     prompt: "select_account"
-  });
+});
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
