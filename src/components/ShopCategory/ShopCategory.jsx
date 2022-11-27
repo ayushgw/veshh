5bf0941ee72c51779441a6afcf3cd14bd541a19e
ayushgw <ayushgoswami@zoho.com>
2022-11-28 02:11:05 +0530

    minor css tweaks

--- src/components/ShopCategory/ShopCategory.jsx
+++ src/components/ShopCategory/ShopCategory.jsx
@@ -17,20 +17,23 @@ const ShopCategory = ({ category }) => {
     }, [category, categoriesMap])
 
     const isCategoryValid = ["mens", "womens", "hats", "sneakers", "jackets"].find(cat => cat === category);
-    if(!isCategoryValid) {
+    if (!isCategoryValid) {
         return (
-            <div style={{textAlign: 'center', marginTop: '6rem'}}>  
-                <WarningImage style={{height: '35px', width: '35px'}} />
-                <div style={{marginTop: '10px', marginBottom: '20px'}}>'/{category}' is not a valid address</div>
-                <Link to="/shop" style={{color: '#5c8fda', marginTop: '20px'}}>Go back to shop</Link>
+            <div style={{ textAlign: 'center', marginTop: '6rem' }}>
+                <WarningImage style={{ height: '35px', width: '35px' }} />
+                <div style={{ marginTop: '10px', marginBottom: '20px' }}>'/{category}' is not a valid address</div>
+                <Link to="/shop" style={{ color: '#5c8fda', marginTop: '20px' }}>Go back to shop</Link>
             </div>
         )
     }
 
     return (
-        <div className='shop-category'>
-            {products && products.map(product => <ProductCard key={product.id} product={product} />)}
-        </div>
+        <>
+            <h2 className='shop-category-title'>{category.toUpperCase()}</h2>
+            <div className='shop-category'>
+                {products && products.map(product => <ProductCard key={product.id} product={product} />)}
+            </div>
+        </>
     )
 }
 
