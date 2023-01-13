0d3944eb0728f2b6f04f4fe28ebcd5bfe6c5a79b
ayushgw <ayushgoswami@zoho.com>
2023-01-13 22:43:16 +0530

    minor changes

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -27,28 +27,12 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const db = getFirestore()
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
-    const collectionRef = collection(db, collectionKey);
-    const batch = writeBatch(db);
-
-    objectsToAdd.forEach((object) => {
-        const docRef = doc(collectionRef, object.title.toLowerCase());
-        batch.set(docRef, object);
-    });
-
-    await batch.commit();
-    console.log('done');
-}
-
 export const getCategoriesAndDocuments = async () => {
     const collectionRef = collection(db, 'categories');
     const q = query(collectionRef);
 
     const querySnapshot = await getDocs(q);
-    const data = querySnapshot.docs.reduce((acc, docSnapshot) => {
-        acc.push(docSnapshot.data())
-        return acc;
-    }, []);
+    const data = querySnapshot.docs.map(docSnapshot => docSnapshot.data())
     
     return data;
 }
--- src/utils/reducer/reducer.js
+++
@@ -1,1 +0,0 @@
-export const CreateAction = (type, payload = null) => ({ type, payload });
